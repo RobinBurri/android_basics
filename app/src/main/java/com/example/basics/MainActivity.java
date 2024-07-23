e5dc22d9b55260ea9f5f685c0c8ced527f20baf2
Robin Burri <robinburri@tutanota.com>
2024-07-23 21:01:13 +0200

    add main activity and gradle files

---
+++ app/src/main/java/com/example/basics/MainActivity.java
@@ -0,0 +1,11 @@
+package com.example.basics;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(android.os.Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
