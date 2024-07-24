05daee939627066c750ab4ad62fb6a76ef26d750
Robin Burri <robinburri@tutanota.com>
2024-07-24 15:51:33 +0200

    add Button and onClickButton

--- app/src/main/java/com/example/basics/MainActivity.java
+++ app/src/main/java/com/example/basics/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.basics;
 
+import android.annotation.SuppressLint;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -7,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(android.os.Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onButtonClick(View view) {
+        android.widget.TextView helloTxt = findViewById(R.id.helloTxt);
+        helloTxt.setText("Hello, Samsung!");
     }
 }
