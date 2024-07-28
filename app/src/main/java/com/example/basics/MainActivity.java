2685a4157c4619bd58b0b70e52783d5f1f137e8f
Robin Burri <robinburri@tutanota.com>
2024-07-28 21:59:38 +0200

    finished UI Basics part2

--- app/src/main/java/com/example/basics/MainActivity.java
+++ app/src/main/java/com/example/basics/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.basics;
 
-import android.annotation.SuppressLint;
 import android.view.View;
-import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,7 +14,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onButtonClick(View view) {
-        android.widget.TextView helloTxt = findViewById(R.id.helloTxt);
-        helloTxt.setText("Hello, Samsung!");
+        TextView helloTxt = findViewById(R.id.helloTxt);
+        EditText nameInput = findViewById(R.id.nameTxt);
+        if (nameInput.getText().toString().isEmpty()) {
+            helloTxt.setText("Hello, Android!!!");
+            return;
+        }
+        helloTxt.setText("Hello, " + nameInput.getText().toString() + "!");
+        nameInput.setText("");
     }
 }
